20219fc74d80d7a0a701869af255b10296d0869c
Serhii Hulko <3834080+felytic@users.noreply.github.com>
2021-12-19 17:35:23 +0100

    Read IFD's tags

--- async_cog/ifd.py
+++ async_cog/ifd.py
@@ -1,7 +1,29 @@
+from typing import List
+
+from PIL.TiffTags import lookup
 from pydantic import BaseModel
+
+
+class Tag(BaseModel):
+    code: int
+    type: int
+    n_values: int
+    pointer: int
+
+    @property
+    def name(self) -> str:
+        tag_info = lookup(self.code)
+        return str(tag_info.name)
+
+    def __repr__(self) -> str:
+        return (
+            f"{self.name}(code: {self.code}, type: {self.type}, "
+            f"n_values: {self.n_values}, pointer: {self.pointer})"
+        )
 
 
 class IFD(BaseModel):
     offset: int
-    n_entries: int
+    n_tags: int
     next_ifd_pointer: int
+    tags: List[Tag]
--- tests/test_cog.py
+++ tests/test_cog.py
@@ -6,7 +6,7 @@ import pytest
 from aioresponses import CallbackResult, aioresponses
 
 from async_cog import COGReader
-from async_cog.ifd import IFD
+from async_cog.ifd import IFD, Tag
 
 
 def test_constructor() -> None:
@@ -79,11 +79,110 @@ async def test_read_ifds() -> None:
 
         async with COGReader(url) as reader:
             assert reader._ifds == [
-                IFD(offset=8, n_entries=13, next_ifd_pointer=4282),
-                IFD(offset=4282, n_entries=14, next_ifd_pointer=4542),
-                IFD(offset=4542, n_entries=14, next_ifd_pointer=4802),
-                IFD(offset=4802, n_entries=14, next_ifd_pointer=5062),
-                IFD(offset=5062, n_entries=14, next_ifd_pointer=0),
+                IFD(
+                    offset=8,
+                    n_tags=13,
+                    next_ifd_pointer=4282,
+                    tags=[
+                        Tag(code=256, type=3, n_values=1, pointer=4194304),
+                        Tag(code=257, type=3, n_values=1, pointer=4194304),
+                        Tag(code=258, type=3, n_values=3, pointer=11141120),
+                        Tag(code=259, type=3, n_values=1, pointer=458752),
+                        Tag(code=262, type=3, n_values=1, pointer=131072),
+                        Tag(code=277, type=3, n_values=1, pointer=196608),
+                        Tag(code=284, type=3, n_values=1, pointer=65536),
+                        Tag(code=322, type=3, n_values=1, pointer=16777216),
+                        Tag(code=323, type=3, n_values=1, pointer=16777216),
+                        Tag(code=324, type=4, n_values=1, pointer=16711680),
+                        Tag(code=325, type=4, n_values=1, pointer=263913472),
+                        Tag(code=339, type=3, n_values=3, pointer=11534336),
+                        Tag(code=347, type=7, n_values=73, pointer=11927552),
+                    ],
+                ),
+                IFD(
+                    offset=4282,
+                    n_tags=14,
+                    next_ifd_pointer=4542,
+                    tags=[
+                        Tag(code=254, type=4, n_values=1, pointer=65536),
+                        Tag(code=256, type=3, n_values=1, pointer=2097152),
+                        Tag(code=257, type=3, n_values=1, pointer=2097152),
+                        Tag(code=258, type=3, n_values=3, pointer=292028416),
+                        Tag(code=259, type=3, n_values=1, pointer=458752),
+                        Tag(code=262, type=3, n_values=1, pointer=131072),
+                        Tag(code=277, type=3, n_values=1, pointer=196608),
+                        Tag(code=284, type=3, n_values=1, pointer=65536),
+                        Tag(code=322, type=3, n_values=1, pointer=8388608),
+                        Tag(code=323, type=3, n_values=1, pointer=8388608),
+                        Tag(code=324, type=4, n_values=1, pointer=348717056),
+                        Tag(code=325, type=4, n_values=1, pointer=148307968),
+                        Tag(code=339, type=3, n_values=3, pointer=292421632),
+                        Tag(code=347, type=7, n_values=73, pointer=292814848),
+                    ],
+                ),
+                IFD(
+                    offset=4542,
+                    n_tags=14,
+                    next_ifd_pointer=4802,
+                    tags=[
+                        Tag(code=254, type=4, n_values=1, pointer=65536),
+                        Tag(code=256, type=3, n_values=1, pointer=1048576),
+                        Tag(code=257, type=3, n_values=1, pointer=1048576),
+                        Tag(code=258, type=3, n_values=3, pointer=309067776),
+                        Tag(code=259, type=3, n_values=1, pointer=458752),
+                        Tag(code=262, type=3, n_values=1, pointer=131072),
+                        Tag(code=277, type=3, n_values=1, pointer=196608),
+                        Tag(code=284, type=3, n_values=1, pointer=65536),
+                        Tag(code=322, type=3, n_values=1, pointer=8388608),
+                        Tag(code=323, type=3, n_values=1, pointer=8388608),
+                        Tag(code=324, type=4, n_values=1, pointer=497025024),
+                        Tag(code=325, type=4, n_values=1, pointer=65273856),
+                        Tag(code=339, type=3, n_values=3, pointer=309460992),
+                        Tag(code=347, type=7, n_values=73, pointer=309854208),
+                    ],
+                ),
+                IFD(
+                    offset=4802,
+                    n_tags=14,
+                    next_ifd_pointer=5062,
+                    tags=[
+                        Tag(code=254, type=4, n_values=1, pointer=65536),
+                        Tag(code=256, type=3, n_values=1, pointer=524288),
+                        Tag(code=257, type=3, n_values=1, pointer=524288),
+                        Tag(code=258, type=3, n_values=3, pointer=326107136),
+                        Tag(code=259, type=3, n_values=1, pointer=458752),
+                        Tag(code=262, type=3, n_values=1, pointer=131072),
+                        Tag(code=277, type=3, n_values=1, pointer=196608),
+                        Tag(code=284, type=3, n_values=1, pointer=65536),
+                        Tag(code=322, type=3, n_values=1, pointer=8388608),
+                        Tag(code=323, type=3, n_values=1, pointer=8388608),
+                        Tag(code=324, type=4, n_values=1, pointer=562298880),
+                        Tag(code=325, type=4, n_values=1, pointer=61276160),
+                        Tag(code=339, type=3, n_values=3, pointer=326500352),
+                        Tag(code=347, type=7, n_values=73, pointer=326893568),
+                    ],
+                ),
+                IFD(
+                    offset=5062,
+                    n_tags=14,
+                    next_ifd_pointer=0,
+                    tags=[
+                        Tag(code=254, type=4, n_values=1, pointer=65536),
+                        Tag(code=256, type=3, n_values=1, pointer=262144),
+                        Tag(code=257, type=3, n_values=1, pointer=262144),
+                        Tag(code=258, type=3, n_values=3, pointer=343146496),
+                        Tag(code=259, type=3, n_values=1, pointer=458752),
+                        Tag(code=262, type=3, n_values=1, pointer=131072),
+                        Tag(code=277, type=3, n_values=1, pointer=196608),
+                        Tag(code=284, type=3, n_values=1, pointer=65536),
+                        Tag(code=322, type=3, n_values=1, pointer=8388608),
+                        Tag(code=323, type=3, n_values=1, pointer=8388608),
+                        Tag(code=324, type=4, n_values=1, pointer=623575040),
+                        Tag(code=325, type=4, n_values=1, pointer=86376448),
+                        Tag(code=339, type=3, n_values=3, pointer=343539712),
+                        Tag(code=347, type=7, n_values=73, pointer=343932928),
+                    ],
+                ),
             ]
 
 
@@ -95,10 +194,113 @@ async def test_read_ifds_big_tiff() -> None:
         mocked_response.get(url, callback=response_read, repeat=True)
 
         async with COGReader(url) as reader:
-            assert reader._ifds == [
-                IFD(offset=16, n_entries=12, next_ifd_pointer=196880),
-                IFD(offset=196880, n_entries=13, next_ifd_pointer=197156),
-                IFD(offset=197156, n_entries=13, next_ifd_pointer=197432),
-                IFD(offset=197432, n_entries=13, next_ifd_pointer=197708),
-                IFD(offset=197708, n_entries=13, next_ifd_pointer=0),
-            ]
+            ifds = reader._ifds
+
+    assert ifds == [
+        IFD(
+            offset=16,
+            n_tags=12,
+            next_ifd_pointer=196880,
+            tags=[
+                Tag(code=256, type=3, n_values=1, pointer=18014398509481984),
+                Tag(code=257, type=3, n_values=1, pointer=18014398509481984),
+                Tag(code=258, type=3, n_values=3, pointer=2251799813685248),
+                Tag(code=259, type=3, n_values=1, pointer=281474976710656),
+                Tag(code=262, type=3, n_values=1, pointer=562949953421312),
+                Tag(code=277, type=3, n_values=1, pointer=844424930131968),
+                Tag(code=284, type=3, n_values=1, pointer=281474976710656),
+                Tag(code=322, type=3, n_values=1, pointer=72057594037927936),
+                Tag(code=323, type=3, n_values=1, pointer=72057594037927936),
+                Tag(code=324, type=16, n_values=1, pointer=76561193665298432),
+                Tag(code=325, type=16, n_values=1, pointer=0),
+                Tag(code=339, type=3, n_values=3, pointer=281474976710656),
+            ],
+        ),
+        IFD(
+            offset=196880,
+            n_tags=13,
+            next_ifd_pointer=197156,
+            tags=[
+                Tag(code=254, type=4, n_values=1, pointer=281474976710656),
+                Tag(code=256, type=3, n_values=1, pointer=9007199254740992),
+                Tag(code=257, type=3, n_values=1, pointer=9007199254740992),
+                Tag(code=258, type=3, n_values=3, pointer=2251799813685248),
+                Tag(code=259, type=3, n_values=1, pointer=281474976710656),
+                Tag(code=262, type=3, n_values=1, pointer=562949953421312),
+                Tag(code=277, type=3, n_values=1, pointer=844424930131968),
+                Tag(code=284, type=3, n_values=1, pointer=281474976710656),
+                Tag(code=322, type=3, n_values=1, pointer=36028797018963968),
+                Tag(code=323, type=3, n_values=1, pointer=36028797018963968),
+                Tag(code=324, type=16, n_values=1, pointer=387309567953862656),
+                Tag(code=325, type=16, n_values=1, pointer=13835058055282163712),
+                Tag(code=339, type=3, n_values=3, pointer=281474976710656),
+            ],
+        ),
+        IFD(
+            offset=197156,
+            n_tags=13,
+            next_ifd_pointer=197432,
+            tags=[
+                Tag(code=254, type=4, n_values=1, pointer=281474976710656),
+                Tag(code=256, type=3, n_values=1, pointer=4503599627370496),
+                Tag(code=257, type=3, n_values=1, pointer=4503599627370496),
+                Tag(code=258, type=3, n_values=3, pointer=2251799813685248),
+                Tag(code=259, type=3, n_values=1, pointer=281474976710656),
+                Tag(code=262, type=3, n_values=1, pointer=562949953421312),
+                Tag(code=277, type=3, n_values=1, pointer=844424930131968),
+                Tag(code=284, type=3, n_values=1, pointer=281474976710656),
+                Tag(code=322, type=3, n_values=1, pointer=36028797018963968),
+                Tag(code=323, type=3, n_values=1, pointer=36028797018963968),
+                Tag(code=324, type=16, n_values=1, pointer=14222367623236026368),
+                Tag(code=325, type=16, n_values=1, pointer=13835058055282163712),
+                Tag(code=339, type=3, n_values=3, pointer=281474976710656),
+            ],
+        ),
+        IFD(
+            offset=197432,
+            n_tags=13,
+            next_ifd_pointer=197708,
+            tags=[
+                Tag(code=254, type=4, n_values=1, pointer=281474976710656),
+                Tag(code=256, type=3, n_values=1, pointer=2251799813685248),
+                Tag(code=257, type=3, n_values=1, pointer=2251799813685248),
+                Tag(code=258, type=3, n_values=3, pointer=2251799813685248),
+                Tag(code=259, type=3, n_values=1, pointer=281474976710656),
+                Tag(code=262, type=3, n_values=1, pointer=562949953421312),
+                Tag(code=277, type=3, n_values=1, pointer=844424930131968),
+                Tag(code=284, type=3, n_values=1, pointer=281474976710656),
+                Tag(code=322, type=3, n_values=1, pointer=36028797018963968),
+                Tag(code=323, type=3, n_values=1, pointer=36028797018963968),
+                Tag(code=324, type=16, n_values=1, pointer=9610681604808638464),
+                Tag(code=325, type=16, n_values=1, pointer=13835058055282163712),
+                Tag(code=339, type=3, n_values=3, pointer=281474976710656),
+            ],
+        ),
+        IFD(
+            offset=197708,
+            n_tags=13,
+            next_ifd_pointer=0,
+            tags=[
+                Tag(code=254, type=4, n_values=1, pointer=281474976710656),
+                Tag(code=256, type=3, n_values=1, pointer=1125899906842624),
+                Tag(code=257, type=3, n_values=1, pointer=1125899906842624),
+                Tag(code=258, type=3, n_values=3, pointer=2251799813685248),
+                Tag(code=259, type=3, n_values=1, pointer=281474976710656),
+                Tag(code=262, type=3, n_values=1, pointer=562949953421312),
+                Tag(code=277, type=3, n_values=1, pointer=844424930131968),
+                Tag(code=284, type=3, n_values=1, pointer=281474976710656),
+                Tag(code=322, type=3, n_values=1, pointer=36028797018963968),
+                Tag(code=323, type=3, n_values=1, pointer=36028797018963968),
+                Tag(code=324, type=16, n_values=1, pointer=4998995586381250560),
+                Tag(code=325, type=16, n_values=1, pointer=13835058055282163712),
+                Tag(code=339, type=3, n_values=3, pointer=281474976710656),
+            ],
+        ),
+    ]
+
+
+def test_tag_repr() -> None:
+    tag = Tag(code=254, type=4, n_values=1, pointer=281474976710656)
+    assert tag.__repr__() == (
+        "NewSubfileType(code: 254, type: 4, n_values: 1, " "pointer: 281474976710656)"
+    )
