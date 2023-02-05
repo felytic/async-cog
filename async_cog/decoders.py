283e7f4505972761545c4c9e2ed66f34c919829d
Serhii Hulko <3834080+felytic@users.noreply.github.com>
2023-02-05 21:54:02 +0200

    Add JPEG decoder

--- async_cog/decoders.py
+++ async_cog/decoders.py
@@ -1,11 +1,12 @@
 from typing import Any, Callable, Dict
 
 import numpy as np
+from imagecodecs import jpeg_decode
 
 from async_cog.ifd import IFD
 
 
-def decode_raw(ifd: IFD, data: bytes) -> Any:
+def decode_raw(ifd: IFD, data: bytes) -> np.ndarray:
     bits_per_sample = ifd["BitsPerSample"]
     width = ifd["TileWidth"]
     height = ifd["TileHeight"]
@@ -20,8 +21,19 @@ def decode_raw(ifd: IFD, data: bytes) -> Any:
     return np.frombuffer(data, dtype=dtype).reshape(*shape)
 
 
+def decode_jpeg(ifd: IFD, data: bytes) -> np.ndarray:
+    jpeg_table = ifd["JPEGTables"]
+
+    # insert tables, first removing the SOI and EOI
+    data = data[0:2] + jpeg_table[2:-2] + data[2:]
+
+    return jpeg_decode(data)
+
+
 Decoder = Callable[[IFD, bytes], Any]
 
 DECODERS_MAPPING: Dict[int, Decoder] = {
     1: decode_raw,
+    6: decode_jpeg,
+    7: decode_jpeg,
 }
